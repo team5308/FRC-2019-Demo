0e1edef8b5911f0e192be7b42f8d2cfeee3e1f10
WuYuanhun <30903332+WuYuanhun@users.noreply.github.com>
2019-01-10 15:42:02 +0800

    modify header

--- src/main/include/subsystems/PID.h
+++ src/main/include/subsystems/PID.h
@@ -1,6 +1,6 @@
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2019-2020 FRC Team 5308. All Rights Reserved.                */
-/* Author: Cetian Li                                                          */
+/* Author: Cetian Liu                                                          */
 /* Date: 2019/01/08                                                           */
 /* Filename: PID.h                                                            */
 /* Project: FRC-2019-Demo                                                     */
@@ -16,6 +16,9 @@
 class PID : public frc::Subsystem {
 
  private:
+  double realDisM; // real Dis
+  double R2Ph; // realDis * R2Ph = targetPhase - basePhase
+
   int basePhase; // Phase reader at the beginning
   int targetPhase; // Target End Phase
   int cPhase; // current distance
